f6deff85b30b5ba1a1935fded3a335481b25ab84
pedrooliveiradelima210402 <pedrooliveiradelima210402@gmail.com>
2025-04-02 02:39:17 +0000

    Criação da lógica de selecionar um botão de gorjetau colocar um valor customizado

--- main.js
+++ main.js
@@ -1,24 +1,31 @@
-let bill = 0
+  let bill = 0
   let tip = 0
   let tipPercentage = 0
   let numberOfPeople = 0
   let buttonSelected = null
-  
-  function receiveBillValue() {
-      bill = document.querySelector("#bill").valueAsNumber
-  }
-  
-  function receiveNumberOfPeopleValue() {
-      numberOfPeople = document.querySelector("#people").valueAsNumber
-  }
-  
+  let bill = 0
   function receiveTipPercentageValue(value) {
       tipPercentage = value / 100
+ 
+      document.querySelector("#custom-tip").value = ""
   
       if (buttonSelected !== null){
           buttonSelected.classList.remove("button-selected")
       }
-  
+ 
       buttonSelected = document.querySelector(`#button-${value}`)
       buttonSelected.classList.add("button-selected")
+  }
+  
+  function receiveCustomTipPercentageValue () {
+      tipPercentage = document.querySelector("#custom-tip").valueAsNumber / 100
+  
+      removeClassButtonSelected()
+  }
+  
+  function removeClassButtonSelected() {
+      if(buttonSelected !== null){
+          buttonSelected.classList.remove("button-selected")
+          buttonSelected = null
+      }
   }
